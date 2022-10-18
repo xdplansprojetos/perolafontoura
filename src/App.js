ff9b38b817d5e8b4958abd8b4e64430d055ee50d
David Xavier <webxddesign@gmail.com>
2022-10-17 23:59:43 -0300

    Correção BUG

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import './App.css';
 import Header from './Components/Layout/Header'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+
 
 
 function App() {
 
-  const Home = () => {
-    return <>
-      <h1>Home</h1>
-    </>
-  }
-
+ 
 
 
   return (
     <>
       <Header />
-      <Router>
-      <div>
-  
-        <Switch>
-         
-          <Route path="/">
-           <Home />
-          </Route>
-          <Route path="/contato">
-           <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+      
 
     </>
   );
