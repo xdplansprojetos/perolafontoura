c934c73c5afc4a108d700c598a907a1a50fd9672
David Xavier <webxddesign@gmail.com>
2022-10-17 23:35:18 -0300

    Perola Codigo Analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Layout/Header'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 function App() {
+
+  const Home = () => {
+    return <>
+      <h1>Home</h1>
+    </>
+  }
+
+
+
   return (
     <>
       <Header />
-      <h1>Site Perola</h1>
+      <Router>
+      <div>
+  
+        <Switch>
+         
+          <Route path="/">
+           <Home />
+          </Route>
+          <Route path="/contato">
+           <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
     </>
   );
 }
