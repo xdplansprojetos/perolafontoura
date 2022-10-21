8b2687027686f255890a42cac3f48bd69a2bdfc5
David Xavier <webxddesign@gmail.com>
2022-10-21 12:11:23 -0300

    Att Windows Carrousel

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
+import Header from './Components/Layout/Header'
+import CarouselSite from './Components/Layout/Carousel';
+import Typography from '@mui/material/Typography';
+
 
 
 function App() {
 
- 
 
 
   return (
+
+
     <div className='fundo'>
 
-    
+      <Header />
+      <CarouselSite />
+      <Typography variant="h1">
+        h1. Heading
+      </Typography>
         
       <h1 className='manutencao'>Site em Manutenção</h1>
 
